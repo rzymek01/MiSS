a53d099754bb2403eb1a0cd33ce1f098d57e5f78
adam <aborowski@atlassian.com>
2015-10-31 13:21:46 +0100

    some bad results

--- Zad2/main.cpp
+++ Zad2/main.cpp
@@ -6,85 +6,34 @@
 #include <vector>
 #include <map>
 #include <cmath>
-
 #include <gmpxx.h>
 
-mpz_class gmpDiv(mpz_class a, mpz_class b) {
-    mpz_class ret;
-    mpz_tdiv_q(ret.get_mpz_t(), a.get_mpz_t(), b.get_mpz_t());
-    return ret;
-}
-
-std::string gmpToString(mpz_class number) {
-    return std::string(mpz_get_str(NULL, 10, number.get_mpz_t()));
-}
-
-struct Frac {
-    mpz_class up;
-    mpz_class down;
-
-    inline bool operator<(const Frac& other) {
-        return (this->up * other.down < this->down * other.up);
-    }
-
-    std::string prec(unsigned long int precision) {
-        double up2 = up.get_d();
-        double down2 = down.get_d();
-        std::string ret = gmpToString(gmpDiv(up * pow(10, precision), down));
 
 
-        return ret;
-    }
-};
+#define uli unsigned long int
+#define mpq_vec std::vector<mpq_class>
+#define uli_vec std::vector<uli>
 
-std::string removeTrailingZeros(std::string &x) {
-    x.erase(x.find_last_not_of('0') + 1, std::string::npos);
-    if ('.' == (*x.rbegin())) {
-        x.erase(x.size() - 1);
-    }
-    return x;
-}
+char buf[100000];
 
-unsigned long int n, d;
-
-void print_mpq(mpq_class mpq) {
-    mpf_class mpq_f(mpq, 100000);
+void print_mpq(mpq_class mpq, uli d) {
+    mpf_class mpq_f(mpq, 100000); // XXX unit of this precision is not clear, I assumed big one is enough
     mpf_ptr mpq_fptr = mpq_f.get_mpf_t();
-    char buf[d+10];
+
     int actualPrinted;
-    gmp_sprintf(buf, "%.*Ff%n", d+1, mpq_fptr, &actualPrinted);
-    printf("%.*s\n", actualPrinted-1, buf);
-    int a = 4;
+    gmp_sprintf(buf, "%.*Ff%n", d + 1, mpq_fptr, &actualPrinted);
+    printf("%.*s\n", actualPrinted - 1, buf);
 }
 
-int main(int argc, char *argv[]) {
-
-    std::string line;
-    std::stringstream converter;
-
-    std::vector<mpq_class> sequence;
-
-    converter << argv[1];
-    converter >> d;
-
-    for (n = 0; std::cin >> line; n++) {
-        mpq_class elem(line);
-        elem.canonicalize();
-        sequence.push_back(elem);
-    }
-
-
-    std::sort(sequence.begin(), sequence.end());
-
+void kolmogorov(mpq_vec& sequence, uli d) {
     mpq_class kPlusMax;
     mpq_class kMinusMax;
+    uli n = sequence.size();
 
     {
-
         unsigned long int j, jBest = 0;
         for (j = 1; j <= n; j++) {
 
-            // j/n - F(x_j) = j/n - a/b = (bj-an)/(bn)
             mpq_class &FXjn = sequence[j - 1];
             mpq_class kPlusCandidate = ((mpq_class) j / (mpq_class) n) - FXjn;
             if (j == 1 || kPlusMax < kPlusCandidate) {
@@ -98,8 +47,93 @@ int main(int argc, char *argv[]) {
     }
 
 
-    print_mpq(kPlusMax);
-    print_mpq(kMinusMax);
+    print_mpq(kPlusMax, d);
+    print_mpq(kMinusMax, d);
+}
+
+class Hi {
+public:
+
+    Hi(/* sorted vec */ mpq_vec& seq, uli d)
+    : seq(seq), d(d), k(10), a(0), b(1), Y() {
+    }
+
+    void execute() {
+        populateY();
+        mpq_class V = calculateV();
+        print_mpq(V, d);
+    }
+private:
+    mpq_vec& seq;
+    uli d;
+    int k;
+    int a, b;
+    uli_vec Y;
+
+    mpq_class getAi(int i) {
+        return (mpq_class) (i * i) / (mpq_class) 100;
+    }
+
+    mpq_class getPi(int i) {
+        return getAi(i) - getAi(i - 1);
+    }
+
+    void populateY() {
+        Y.assign(k + 1, 0);
+        //iterate over seq and count
+        uli currentI = 0;
+        mpq_class bound = getAi(currentI);
+        for (mpq_vec::iterator it = seq.begin(); it != seq.end(); ++it) {
+            /* std::cout << *it; ... */
+            double _it = it->get_d();
+            double _bound = bound.get_d();
+//            printf("OUTER it: %f bound: %f\n", _it, _bound);
+            while (*it > bound) {
+                currentI++;
+                bound = getAi(currentI);
+                _bound = bound.get_d();
+//                printf("INNER it: %f bound: %f\n", _it, _bound);
+            }
+//            printf("element %f qualified into Y%d (<=%f)\n", _it, currentI, _bound);
+            Y[currentI]++;
+        }
+    }
+
+    mpq_class calculateV() {
+        mpq_class n = seq.size();
+        mpq_class V = 0;
+        for (int i = 1; i <= k; i++) {
+            mpq_class Pi = getPi(i);
+            mpq_class up = Y[i] - n*Pi;
+            V += (up * up) / (n * Pi);
+        }
+        return V;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    uli d;
+    std::string segment;
+    std::stringstream converter;
+
+    mpq_vec sequence;
+    sequence.reserve(10000);
+
+    converter << argv[1];
+    converter >> d;
+
+    while (std::cin >> segment) {
+        mpq_class elem(segment);
+        elem.canonicalize();
+        sequence.push_back(elem);
+    }
+
+    std::sort(sequence.begin(), sequence.end());
+
+    Hi hiExecutor(sequence, d);
+
+    hiExecutor.execute();
+    kolmogorov(sequence, d);
 
     return 0;
 }

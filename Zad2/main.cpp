624a35599838836c5be77847f6a3d2c1bcf2f4af
adam <aborowski@atlassian.com>
2015-10-31 11:35:29 +0100

    k+ and k- works

--- Zad2/main.cpp
+++ Zad2/main.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 #include <map>
 #include <cmath>
-#include <gmp.h>
+
 #include <gmpxx.h>
 
 mpz_class gmpDiv(mpz_class a, mpz_class b) {
@@ -45,72 +45,61 @@ std::string removeTrailingZeros(std::string &x) {
     return x;
 }
 
+unsigned long int n, d;
+
+void print_mpq(mpq_class mpq) {
+    mpf_class mpq_f(mpq, 100000);
+    mpf_ptr mpq_fptr = mpq_f.get_mpf_t();
+    char buf[d+10];
+    int actualPrinted;
+    gmp_sprintf(buf, "%.*Ff%n", d+1, mpq_fptr, &actualPrinted);
+    printf("%.*s\n", actualPrinted-1, buf);
+    int a = 4;
+}
+
 int main(int argc, char *argv[]) {
-    unsigned long int n, d;
-    mpz_class acc, acc2, tmp, multiplicand, quotient;
-    std::string x;
-    std::stringstream convert;
 
-    typedef std::map<unsigned long int, unsigned long int>::iterator pos_map_it;
-    std::vector<Frac> sequence;
-    unsigned long int period = 0;
+    std::string line;
+    std::stringstream converter;
 
-    convert << argv[1];
-    convert >> d;
+    std::vector<mpq_class> sequence;
 
-    acc = 0;
-    acc2 = 0;
-    tmp = 0;
-    multiplicand = 0;
-    quotient = 0;
+    converter << argv[1];
+    converter >> d;
 
-    for (n = 0; std::cin >> x; n++) {
-        // prase fraction
-        size_t a = x.find('/');
-        Frac elem;
-        elem.up = x.substr(0, a);
-        elem.down = x.substr(a + 1, -1);
-        //put fraction into sequence
+    for (n = 0; std::cin >> line; n++) {
+        mpq_class elem(line);
+        elem.canonicalize();
         sequence.push_back(elem);
-
     }
 
 
     std::sort(sequence.begin(), sequence.end());
 
-    Frac *max = NULL;
+    mpq_class kPlusMax;
+    mpq_class kMinusMax;
+
     {
 
         unsigned long int j, jBest = 0;
         for (j = 1; j <= n; j++) {
 
             // j/n - F(x_j) = j/n - a/b = (bj-an)/(bn)
-            Frac &FXjn = sequence[j - 1];
-            mpz_class &a = FXjn.up;
-            mpz_class &b = FXjn.down;
-            Frac newFrac; // (bj-an) / (bn))
-            newFrac.up = b * j - a*n;
-            //todo sometimes it gets 0 which is weird...
-            newFrac.down = b*n;
-            double up2 = newFrac.up.get_d();
-            double down2 = newFrac.down.get_d();
-            if (max == NULL || *max < newFrac) {
-                max = &newFrac;
-                jBest = j;
+            mpq_class &FXjn = sequence[j - 1];
+            mpq_class kPlusCandidate = ((mpq_class) j / (mpq_class) n) - FXjn;
+            if (j == 1 || kPlusMax < kPlusCandidate) {
+                kPlusMax = kPlusCandidate;
             }
-
+            mpq_class kMinusCandidate = FXjn - (((mpq_class) j - 1) / (mpq_class) n);
+            if (j == 1 || kMinusMax < kMinusCandidate) {
+                kMinusMax = kMinusCandidate;
+            }
         }
-        int a = 5;
     }
 
 
-    std::cout << max->prec(d);
-
-
-
-    for (std::vector<Frac>::iterator i = sequence.begin(); i != sequence.end(); ++i)
-        std::cout << gmpToString(i->up) << '/' << gmpToString(i->down) << " = " << gmpToString(gmpDiv(i->up * 10000, i->down)) << std::endl;
-
+    print_mpq(kPlusMax);
+    print_mpq(kMinusMax);
 
     return 0;
 }
